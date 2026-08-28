94c08320a42e0be0870b611d99288523b74d2d27
Erik Smistad <ersmistad@gmail.com>
2026-08-28 11:49:46 +0200

    Added callback functionality to InputTextWidget

--- source/FAST/Visualization/Widgets/InputTextWidget/InputTextWidget.cpp
+++ source/FAST/Visualization/Widgets/InputTextWidget/InputTextWidget.cpp
@@ -6,27 +6,14 @@
 
 namespace fast {
 
-InputTextWidget::InputTextWidget(std::string title, std::string text, bool singleLine, QWidget *parent) : QWidget(parent) {
-    m_singleLine = singleLine;
-    m_text = text;
-    m_title = title;
-    // Use QueuedConnection to assure slot is called in main thread
-    if(singleLine) {
-        m_inputWidget = new QLineEdit(text.c_str());
-        connect(this, &InputTextWidget::updateTextSignal, (QLineEdit*)m_inputWidget, &QLineEdit::setText, Qt::QueuedConnection);
-        connect((QLineEdit*)m_inputWidget, &QLineEdit::textChanged, [=](QString str) { setText(str.toStdString()); });
-    } else {
-        m_inputWidget = new QPlainTextEdit(text.c_str());
-        connect(this, &InputTextWidget::updateTextSignal, (QPlainTextEdit*)m_inputWidget, &QPlainTextEdit::setPlainText, Qt::QueuedConnection);
-        connect((QPlainTextEdit*)m_inputWidget, &QPlainTextEdit::textChanged, [=]() { setText(((QPlainTextEdit*)m_inputWidget)->toPlainText().toStdString()); });
-    }
-    auto layout = new QVBoxLayout;
-    if(!m_title.empty()) {
-        m_label = new QLabel(m_title.c_str());
-        layout->addWidget(m_label);
-    }
-    layout->addWidget(m_inputWidget);
-    setLayout(layout);
+InputTextWidget::InputTextWidget(std::string title, std::string text, bool singleLine, std::function<void(std::string)> callback, QWidget *parent) : QWidget(parent) {
+    m_callbackFunction = callback;
+    init(title, text, singleLine);
+}
+
+InputTextWidget::InputTextWidget(std::string title, std::string text, bool singleLine, InputTextWidgetCallback* callback, QWidget *parent) : QWidget(parent) {
+    m_callbackClass = callback;
+    init(title, text, singleLine);
 }
 
 void InputTextWidget::setText(std::string text) {
@@ -43,6 +30,44 @@ std::string InputTextWidget::getText() {
     }
 
     return text;
+}
+
+void InputTextWidget::init(std::string title, std::string text, bool singleLine) {
+    m_singleLine = singleLine;
+    m_text = text;
+    m_title = title;
+    // Use QueuedConnection to assure slot is called in main thread
+    if(singleLine) {
+        m_inputWidget = new QLineEdit(text.c_str());
+        connect(this, &InputTextWidget::updateTextSignal, (QLineEdit*)m_inputWidget, &QLineEdit::setText, Qt::QueuedConnection);
+        connect((QLineEdit*)m_inputWidget, &QLineEdit::textChanged, [=](QString str) {
+            setText(str.toStdString());
+            if(m_callbackClass != nullptr) {
+                m_callbackClass->handle(str.toStdString());
+            } else {
+                m_callbackFunction(str.toStdString());
+            }
+        });
+    } else {
+        m_inputWidget = new QPlainTextEdit(text.c_str());
+        connect(this, &InputTextWidget::updateTextSignal, (QPlainTextEdit*)m_inputWidget, &QPlainTextEdit::setPlainText, Qt::QueuedConnection);
+        connect((QPlainTextEdit*)m_inputWidget, &QPlainTextEdit::textChanged, [=]() {
+            QString str =((QPlainTextEdit*)m_inputWidget)->toPlainText();
+            setText(str.toStdString());
+            if(m_callbackClass != nullptr) {
+                m_callbackClass->handle(str.toStdString());
+            } else {
+                m_callbackFunction(str.toStdString());
+            }
+        });
+    }
+    auto layout = new QVBoxLayout;
+    if(!m_title.empty()) {
+        m_label = new QLabel(m_title.c_str());
+        layout->addWidget(m_label);
+    }
+    layout->addWidget(m_inputWidget);
+    setLayout(layout);
 }
 
 }
--- source/FAST/Visualization/Widgets/InputTextWidget/InputTextWidget.hpp
+++ source/FAST/Visualization/Widgets/InputTextWidget/InputTextWidget.hpp
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <FASTExport.hpp>
 #include <mutex>
+#include <iostream>
 
 class QLabel;
 
@@ -16,7 +17,34 @@ namespace fast {
     ~InputTextWidget() {
     }
 };
+
+%feature("director") InputTextWidgetCallback;
+%pythoncode %{
+_input_text_callbacks = [] # Hack to avoid callbacks being deleted
+def InputTextCallback(func):
+    global _input_text_callbacks
+    class CB(InputTextWidgetCallback):
+        def __init__(self):
+            super().__init__()
+
+        def handle(self, value):
+            func(value)
+    obj = CB()
+    _input_text_callbacks.append(obj)
+    return obj
+%}
 #endif
+
+
+/**
+ * @brief Callback class for InputTextWidget
+ * Used primarily by pyFAST
+ */
+class InputTextWidgetCallback {
+public:
+    virtual void handle(std::string text) = 0;
+    virtual ~InputTextWidgetCallback() { std::cout << "Destroying InputTextWidgetCallback" << std::endl;};
+};
 
 /**
  * @brief A input text widget
@@ -25,14 +53,26 @@ namespace fast {
 class FAST_EXPORT InputTextWidget : public QWidget {
     Q_OBJECT
     public:
+#ifndef SWIG
         /**
          * @brief Create an input text widget
          * @param title Title of input text widget (can contain HTML)
          * @param text Initial text of input text widget
          * @param singleLine Whether to use a single line input text widget or a multi-line widget
+         * @param callback Callback function for when text is changed
          * @param parent
          */
-        InputTextWidget(std::string title = "", std::string text = "", bool singleLine = true, QWidget* parent = nullptr);
+        InputTextWidget(std::string title = "", std::string text = "", bool singleLine = true, std::function<void(std::string)> callback = {}, QWidget* parent = nullptr);
+#endif
+        /**
+         * @brief Create an input text widget
+         * @param title Title of input text widget (can contain HTML)
+         * @param text Initial text of input text widget
+         * @param singleLine Whether to use a single line input text widget or a multi-line widget
+         * @param callback Callback for when text is changed
+         * @param parent
+         */
+        InputTextWidget(std::string title = "", std::string text = "", bool singleLine = true, InputTextWidgetCallback* callback = nullptr, QWidget* parent = nullptr);
         /**
          * @brief Set text
          * @param text
@@ -48,12 +88,16 @@ class FAST_EXPORT InputTextWidget : public QWidget {
         void updateTextSignal(QString text);
 #endif
     private:
+        void init(std::string title, std::string text, bool singleLine);
         QWidget* m_inputWidget;
         QLabel* m_label;
         bool m_singleLine = true;
         std::string m_text;
         std::string m_title;
         std::mutex m_mutex;
+
+        std::function<void(std::string)> m_callbackFunction;
+        InputTextWidgetCallback* m_callbackClass = nullptr;
 };
 
 
